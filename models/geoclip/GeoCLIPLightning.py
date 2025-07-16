e793b23d2b5323bc830a9779441345915bdfb572
PHChenGit <chenpohsun1@gmail.com>
2025-07-16 15:56:05 +0800

    feat: Import superglue, lightglue, sift, orb

--- models/geoclip/GeoCLIPLightning.py
+++ models/geoclip/GeoCLIPLightning.py
@@ -11,6 +11,10 @@ from torch.optim.lr_scheduler import CosineAnnealingLR
 import pytorch_lightning as pl
 from transformers import CLIPModel, AutoProcessor
 from PIL import Image as IM
+from models.SuperGlue.match_pair import superglue_matching
+from models.LightGlue.match_pair import lightglue_matching
+from models.LightGlue.lightglue.lightglue import LightGlue
+from models.LightGlue.lightglue.superpoint import SuperPoint
 
 from .image_encoder import ImageEncoder
 from .location_encoder import LocationEncoder
@@ -20,6 +24,7 @@ from .misc import (
     estimate_rotation_angle,
     crop_image,
 )
+
 
 class GeoCLIPLightning(pl.LightningModule):
     def __init__(self,
@@ -49,21 +54,28 @@ class GeoCLIPLightning(pl.LightningModule):
             param.requires_grad = False
 
         self.criterion = nn.CrossEntropyLoss()
-        """
-        For saving prediction result and ground-truth in prediction step
-        """
-        self.pred_coordinate_list = []
-        self.true_coordinate_list = []
-        self.pred_yaw_list = []
-        self.true_yaw_list = []
+
         """
         Only work at prediction stage.
         For estimate uav orientation and refine coordinates
         """
-        self.mapglue = torch.jit.load(Path('~/Documents/hsun/NavCLIP/models/MapGlue/weights/fastmapglue_model.pt').expanduser())
-        self.mapglue.eval()
-        if sat_img:
-            self.sat_img = IM.open(sat_img).convert('RGB')
+        if homography_method == 'mapglue':
+            self.feat_matching_model = torch.jit.load(Path('~/Documents/hsun/NavCLIP/models/MapGlue/weights/fastmapglue_model.pt').expanduser())
+            self.feat_matching_model.eval()
+        elif homography_method == 'lightglue':
+            # Initialize feature extractor and matcher
+            self.feature_extractor = SuperPoint(max_num_keypoints=2048).eval()
+            self.lightglue_matcher = LightGlue(features='superpoint').eval()
+        elif self.hparams.homography_method == 'sift':
+            print('sift')
+            self.homography_method = cv2.SIFT_create()
+            self.matcher = cv2.BFMatcher(cv2.NORM_L2)
+        elif self.hparams.homography_method == 'orb':
+            print('orb')
+            self.homography_method = cv2.ORB_create()
+            self.matcher = cv2.BFMatcher(cv2.NORM_HAMMING)
+        
+        self.sat_img = IM.open(sat_img).convert('RGB')
 
     def load_weights(self, pretrained_dir):
         """Loads weights to the correct device"""
@@ -111,6 +123,7 @@ class GeoCLIPLightning(pl.LightningModule):
     def encode_image(self, image):
         with torch.no_grad(): # 通常 CLIP 主幹不訓練
             image_embeddings = self.CLIP.get_image_features(pixel_values=image) # [B, 768]
+            # image_embeddings_2 = self.CLIP.get_image_features(pixel_values=img2) # [B, 768]
         image_features = self.image_encoder(image_embeddings) # [B, 512]
         image_features = F.normalize(image_features, dim=1)
         return image_features
@@ -183,7 +196,7 @@ class GeoCLIPLightning(pl.LightningModule):
         return val_dist_mae
 
     def predict_step(self, batch, batch_idx, dataloader_idx=0):
-        sat_img, query_imgs, gps_coordinate_3d, pixel_coordinate_3d = batch
+        sat_img, query_imgs, gps_coordinate_3d, pixel_coordinate_3d, drone_filename = batch
         yaws = pixel_coordinate_3d[:, 2].detach().cpu().numpy()
 
         if self.gps_gallery.device != self.device:
@@ -200,32 +213,117 @@ class GeoCLIPLightning(pl.LightningModule):
 
         restored_query_img_pil: IM.Image = denormalize_and_restore_image(query_imgs)[0] # [224, 224]
         restored_ref_img_pil: IM.Image = denormalize_and_restore_image(sat_img)[0] # [224, 224]
-        pred_sat_crop_pil: IM.Image = crop_image(self.sat_img, (best_pred_coordinates[0], best_pred_coordinates[1]), (600, 600))
+        pred_sat_crop_pil: IM.Image = crop_image(self.sat_img, (best_pred_coordinates[0], best_pred_coordinates[1]), (224, 224))
+        # pred_sat_crop_pil.save(f"/home/rvl1421/Documents/hsun/datasets/DJI_NTU_5/matching_kpt_1/pred_sat_images/{drone_filename}.png")
 
         uav_img_np = np.array(restored_query_img_pil)
+        # pred_sat_crop_tensor = self.glue_aug(pred_sat_crop_pil, return_tensor=True)
         pred_sat_crop_np = np.array(pred_sat_crop_pil)
         ref_sat_img_np = np.array(restored_ref_img_pil)
+        best_yaw_pred = np.nan
 
-        try:
-            best_H_pred, best_yaw_pred, best_matches = estimate_rotation_angle(self.mapglue, uav_img_np, pred_sat_crop_np)
-        except Exception as e:
-            print(f"pred image not found matches, {e}")
-            best_matches = np.array([])
-            best_H_pred = None
+        if self.hparams.homography_method == 'mapglue':
+            try:
+                best_H_pred, best_yaw_pred, best_matches, pt0, pt1 = estimate_rotation_angle(self.mapglue, uav_img_np, pred_sat_crop_np)
+
+                if best_yaw_pred >= 20:
+                    pt0_np = pt0.cpu().numpy()
+                    pt1_np = pt1.cpu().numpy()
+                    keypoints0 = [cv2.KeyPoint(p[0], p[1], 1) for p in pt0_np]
+                    keypoints1 = [cv2.KeyPoint(p[0], p[1], 1) for p in pt1_np]
+
+                    inlier_indices = np.where(best_matches.flatten() > 0)[0]
+                    matches_to_draw = [cv2.DMatch(i, i, 0) for i in inlier_indices]
+
+                    matched_kpt_img = cv2.drawMatches(
+                            uav_img_np, 
+                            keypoints0, 
+                            pred_sat_crop_np, 
+                            keypoints1, 
+                            matches_to_draw, # Pass the list of DMatch objects for inliers
+                            None, 
+                            flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS
+                        )
+                    cv2.imwrite(f"/home/rvl1421/Documents/hsun/datasets/DJI_NTU_5/matching_kpt_1/{drone_filename}_{best_pred_coordinates[0]}_{best_pred_coordinates[1]}_{pixel_coordinate_3d[0, 0]}_{pixel_coordinate_3d[0, 1]}.png", matched_kpt_img)
+            except Exception as e:
+                print(f"pred image not found matches, {e}")
+                best_matches = np.array([])
+                best_H_pred = None
+        elif self.hparams.homography_method == 'superglue':
+            try:
+                best_H_pred, best_yaw_pred = superglue_matching(uav_img_np, pred_sat_crop_np)
+            except Exception as e:
+                print(f"pred image not found matches, {e}")
+                best_matches = np.array([])
+                best_H_pred = None
+        elif self.hparams.homography_method == 'lightglue':
+            best_H_pred, best_yaw_pred = lightglue_matching(uav_img_np, pred_sat_crop_np, self.feature_extractor, self.lightglue_matcher, self.device)
+        elif self.hparams.homography_method in ['sift', 'orb']:
+            gray_uav = cv2.cvtColor(uav_img_np, cv2.COLOR_RGB2GRAY)
+            gray_sat = cv2.cvtColor(ref_sat_img_np, cv2.COLOR_RGB2GRAY)
+            
+            # Detect keypoints and compute descriptors
+            kp1, des1 = self.homography_method.detectAndCompute(gray_uav, None)
+            kp2, des2 = self.homography_method.detectAndCompute(gray_sat, None)
+
+            if des1 is None or des2 is None:
+                raise Exception("No descriptors found for one or both images.")
+            
+            # Match descriptors
+            matches = self.matcher.knnMatch(des1, des2, k=2)
+            
+            # Apply Lowe's ratio test
+            good_matches = []
+            # print(f"matches: {matches}, len of matches: {len(matches[0])}")
+            if matches and len(matches) > 0 and len(matches[0]) == 2:
+                for m, n in matches:
+                    if m.distance < 0.9 * n.distance:
+                        good_matches.append(m)
+            
+            if len(good_matches) < 4: # A common threshold for robust estimation
+                raise Exception(f"Not enough good matches found - only {len(good_matches)}.")
+            
+            # Extract location of good matches
+            src_pts = np.float32([ kp1[m.queryIdx].pt for m in good_matches ]).reshape(-1,1,2)
+            dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good_matches ]).reshape(-1,1,2)
+            
+            # Find homography
+            H, _ = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+            if H is None:
+                raise Exception("findHomography returned None.")
+            
+            best_H_pred = H
+            # Calculate yaw from homography
+            A = best_H_pred[0:2, 0:2]
+            a, b, c, d = A[0, 0], A[0, 1], A[1, 0], A[1, 1]
+            yaw_rad = np.arctan2(c, d) if np.isclose(a, d) and np.isclose(-b, c) else np.arctan2(-b, a)
+            best_yaw_pred = np.rad2deg(yaw_rad)
+
+            if best_yaw_pred >= 20:
+                matched_kpt_img = cv2.drawMatches(
+                                uav_img_np, 
+                                kp1, 
+                                pred_sat_crop_np, 
+                                kp2, 
+                                good_matches, # Pass the list of DMatch objects for inliers
+                                None, 
+                                flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS
+                            )
+                cv2.imwrite(f"/home/rvl1421/Documents/hsun/NavCLIP/output/openai_clip_image_encoder_sa_and_gelu_with_DJI_NTU_5/test/route_5_sift/{drone_filename}_{best_pred_coordinates[0]}_{best_pred_coordinates[1]}_{pixel_coordinate_3d[0, 0]}_{pixel_coordinate_3d[0, 1]}.png", matched_kpt_img)
 
         if best_H_pred is None:
             return {
                 "pred_pixel_coordinate": best_pred_coordinates,
-                "true_pixel_coordinate": pixel_coordinate_3d[:, :2].detach().cpu().numpy(),
-                "true_gps_coordinate": gps_coordinate_3d[:, :2].detach().cpu().numpy(),
+                "true_pixel_coordinate": pixel_coordinate_3d[0, :2].detach().cpu().numpy(),
+                "true_gps_coordinate": gps_coordinate_3d[0, :2].detach().cpu().numpy(),
                 "pred_yaw_angle": np.nan,
                 "true_yaw": yaws
             }
 
         return {
             "pred_pixel_coordinate": best_pred_coordinates,
-            "true_pixel_coordinate": pixel_coordinate_3d[:, :2].detach().cpu().numpy(),
-            "true_gps_coordinate": gps_coordinate_3d[:, :2].detach().cpu().numpy(),
+            "true_pixel_coordinate": pixel_coordinate_3d[0, :2].detach().cpu().numpy(),
+            "true_gps_coordinate": gps_coordinate_3d[0, :2].detach().cpu().numpy(),
             "pred_yaw_angle": best_yaw_pred,
             "true_yaw": yaws
         }
